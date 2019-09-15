89c6351f22151e2bc5ce82e15bcb9076d109b513
Patricio Daniel Giusti <54557743+giustipatricio@users.noreply.github.com>
2019-09-14 22:34:52 -0300

    actualizacion
    actualizacion

---
+++ Nav/nav.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="navCss.css" />
+    <title>- - - - - We Brew- - - - - </title>
+    </head>
+        <nav class="navbar navbar-fixed-top"role="navigation">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">WE BREW</a> <!--!agregar imagen!-->
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                  <li><a href="#">HOME</a></li>
+                  <li><a href="#">PREGUNTAS FRECUENTES</a></li>
+                  <li><a href="#">CONTACTO</a></li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                    role="button" aria-haspopup="true" aria-expanded="false">PRODUCTOS <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">Porrones</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Barriles</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Growlers</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Artesanales</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Accesorios</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="navbar-form navbar-left">
+                  <div class="form-group">
+                    <input type="text" class="form-control" placeholder="¿ Qué estás buscando ?">
+                  </div>
+                  <button type="submit" class="btn btn-default">Tuyo!</button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">LOGIN <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="#">LOGIN</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">REGISTRO</a></li>
+                    </ul>
+                  </li>
+                  <li id="market"><a> <i class="fas fa-shopping-cart"></i></a></li>
+                </ul>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+            </nav>
+            <img src="woodsfondo.jpg">
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+ <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+ <!-- Include all compiled plugins (below), or include individual files as needed -->
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+ <script src="https://use.fontawesome.com/releases/v5.10.2/js/all.js" data-auto-replace-svg="nest"></script>
+  </body>
+</html>
