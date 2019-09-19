780655f0addda471f4f6a4da66e1970cbc06ad88
Patricio Giusti <giusti.patricio@gmail.com>
2019-09-19 19:19:44 -0300

    agregado Navloged e iniciado perfil.php

---
+++ Perfil/perfil.php
@@ -0,0 +1,283 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="../fotosComunes/webrewhead.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="perfil.css" />
+    <link rel="stylesheet" type="text/css" href="../Footer/Footer.css" />
+    <link rel="stylesheet" type="text/css" href="../Navloged/navCss.css" />
+    <title>- - - - - We Brew- - - - - </title>
+  </head>
+          <?php require "../Navloged/nav.php" ?>
+          <div class="espacio" style="padding-top:3vw"></div>
+          <div class="container" id="singin" style="text-align: -webkit-center; padding:0px; margin:0 auto">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <div class="titulo" style="text-align-last: center;"><h2>Crea tu Cuenta, es fácil y rápido</h2></label>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputEmail3" class="col-sm-2 control-label">Reescríbelo</label>
+                <div class="col-sm-10">
+                  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputPassword3" class="col-sm-2 control-label">Contraseña</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+              </div>
+              <div class="espacio" style="padding-top: 20px; padding-bottom: 10px"></div>
+              <div class="titulo" style="text-align-last: left;"><h4>Fecha de nacimiento</h4></label>
+              </div>
+              <div class="form-group">
+                <label for="dia" class="col-sm-4 control-label">Día
+                  <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    <option>21</option>
+                    <option>22</option>
+                    <option>23</option>
+                    <option>24</option>
+                    <option>25</option>
+                    <option>26</option>
+                    <option>27</option>
+                    <option>28</option>
+                    <option>29</option>
+                    <option>30</option>
+                    <option>31</option>
+                  </select>
+                </label>
+                <label for="Mes" class="col-sm-4 control-label">Mes
+                  <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                  </select>
+                </label>
+                <label for="ano" class="col-sm-4 control-label">Año
+                  <select>
+                    <option>2001</option>
+                    <option>2000</option>
+                    <option>1999</option>
+                    <option>1998</option>
+                    <option>1997</option>
+                    <option>1996</option>
+                    <option>1995</option>
+                    <option>1994</option>
+                    <option>1993</option>
+                    <option>1992</option>
+                    <option>1991</option>
+                    <option>1990</option>
+                    <option>1989</option>
+                    <option>1988</option>
+                    <option>1987</option>
+                    <option>1986</option>
+                    <option>1985</option>
+                    <option>1984</option>
+                    <option>1983</option>
+                    <option>1982</option>
+                    <option>1981</option>
+                    <option>1980</option>
+                    <option>1979</option>
+                    <option>1978</option>
+                    <option>1977</option>
+                    <option>1976</option>
+                    <option>1975</option>
+                    <option>1974</option>
+                    <option>1973</option>
+                    <option>1972</option>
+                    <option>1971</option>
+                    <option>1970</option>
+                    <option>1969</option>
+                    <option>1968</option>
+                    <option>1967</option>
+                    <option>1966</option>
+                    <option>1965</option>
+                    <option>1964</option>
+                    <option>1963</option>
+                    <option>1962</option>
+                    <option>1961</option>
+                    <option>1960</option>
+                    <option>1959</option>
+                    <option>1958</option>
+                    <option>1957</option>
+                    <option>1956</option>
+                    <option>1955</option>
+                    <option>1954</option>
+                    <option>1953</option>
+                    <option>1952</option>
+                    <option>1951</option>
+                    <option>1950</option>
+                    <option>1949</option>
+                    <option>1948</option>
+                    <option>1947</option>
+                    <option>1946</option>
+                    <option>1945</option>
+                    <option>1944</option>
+                    <option>1943</option>
+                    <option>1942</option>
+                    <option>1941</option>
+                    <option>1940</option>
+                  </select>
+                </label>
+              </div>
+              <div class="espacio" style="padding-top: 20px; padding-bottom: 10px"></div>
+              <div class="titulo" style="text-align-last: left;"><h4>Datos Personales</h4></label></div>
+              <div class="form-group">
+                <label for="Nombre" class="col-sm-2 control-label">Nombre</label>
+                <div class="col-sm-10">
+                  <input type="Nombre" class="form-control" id="Nombre" placeholder="Nombre">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="Apellido" class="col-sm-2 control-label">Apellido</label>
+                <div class="col-sm-10">
+                  <input type="Apellido" class="form-control" id="Apellido" placeholder="Apellido">
+                </div>
+              </div>
+              <label for="Sexo" class="col-sm-2 control-label">Sexo</label>
+              <p>
+              <label class="radio-inline">
+                <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Mujer
+              </label>
+              <label class="radio-inline">
+                <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Hombre
+              </label>
+              <label class="radio-inline">
+                <input type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3"> Personalizado
+              </label>
+              <div class="espacio" style="padding-top: 20px; padding-bottom: 10px"></div>
+              <div class="titulo" style="text-align-last: left;"><h4>Dirección</h4></label></div>
+              <div class="form-group">
+                <label for="direccion" class="col-sm-2 control-label">Calle</label>
+                <div class="col-sm-10">
+                  <input type="Calle" class="form-control" id="Calle" placeholder="Calle">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="numdireccion" class="col-sm-2 control-label">Número</label>
+                <div class="col-sm-10">
+                  <input type="Numero" class="form-control" id="Numero" placeholder="Número">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="Pisodireccion" class="col-sm-2 control-label">Piso</label>
+                <div class="col-sm-10">
+                  <input type="Piso" class="form-control" id="Piso" placeholder="Piso">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="dia" class="col-sm-offset-2 col-sm-2 control-label">País
+                  <select>
+                    <option>Argentina</option>
+                  </select>
+                </label>
+                <label for="Provincia" class="col-sm-offset-2 col-sm-2 control-label">Provincia
+                  <select>
+                    <option>Buenos Aires</option>
+                    <option>CABA</option>
+                    <option>Catamarca</option>
+                    <option>Chaco</option>
+                    <option>Chubut</option>
+                    <option>Córdoba</option>
+                    <option>Corrientes</option>
+                    <option>Entre Ríos</option>
+                    <option>Formosa</option>
+                    <option>Jujuy</option>
+                    <option>La Pampa</option>
+                    <option>La Rioja</option>
+                    <option>Mendoza</option>
+                    <option>Misiones</option>
+                    <option>Neuquén</option>
+                    <option>Río Negro</option>
+                    <option>Salta</option>
+                    <option>San Juan</option>
+                    <option>San Luis</option>
+                    <option>Santa Cruz</option>
+                    <option>Santa Fe</option>
+                    <option>Santiago del Estero</option>
+                    <option>Tierra del Fuego</option>
+                    <option>Tucumán</option>
+                  </select>
+                </label>
+              </div>
+              <div class="form-group">
+                <label for="Ciudad" class="col-sm-2 control-label">Ciudad</label>
+                <div class="col-sm-10">
+                  <input type="Ciudad" class="form-control" id="Ciudad" placeholder="Ciudad">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="Codigo Postal" class="col-sm-2 control-label">Código Postal</label>
+                <div class="col-sm-10">
+                  <input type="Codigo Postal" class="form-control" id="Codigo Postal" placeholder="Código Postal">
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox"> Acepto <a> términos y condiciones </a>
+                    </label>
+                    <label>
+                      <input type="checkbox"> Deseo recibir actualizaciones
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-offset-4 col-sm-8">
+                <button type="submit" class="btn btn-default" style="width:50%">Crear Cuenta</button>
+              </div>
+            </form>
+          </div>
+          </div>
+          <?php require "../Footer/Footer.php" ?>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+ <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+ <!-- Include all compiled plugins (below), or include individual files as needed -->
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+ <script src="https://use.fontawesome.com/releases/v5.10.2/js/all.js" data-auto-replace-svg="nest"></script>
+  </body>
+</html>
