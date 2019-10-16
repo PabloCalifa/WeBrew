780c6ef33c47c06ecb24211f7cf218b4517c2a37
Pablo <pablocalifano@hotmail.com>
2019-10-16 18:35:10 -0300

    index

--- index/index.php
+++ index/index.php
@@ -1,91 +1,78 @@
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-<body>
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="../fotosComunes/webrewhead.png">
     <link rel="stylesheet" type="text/css" href="../SingIn/SingIn.css" />
     <link rel="stylesheet" type="text/css" href="../Footer/Footer.css" />
     <link rel="stylesheet" type="text/css" href="../Nav/navCss.css" />
     <link rel="stylesheet" type="text/css" href="incss.css" />
-    <link rel="icon" type="image/png" href="../fotosComunes/webrewhead.png">
     <title>  We Brew</title>
-
-
-    </head>
-          <?php require "../Nav/nav.php" ?>
-  <section id="seccion">
-
-    <div id="promo" class="row">
-<div class="col-md-4 col-sm-4 col-xs-12" style="padding-top:10px"><a href="#" target="_blank"><img alt="Financiación" class="img-responsive" src="productos/Financiacion.jpg"></a></div>
-
-<div class="col-md-4 col-sm-4 col-xs-12" style="padding-top:10px"><a href="#" target="_blank"><img alt="Video instructivo" class="img-responsive" src="productos/tutorial.jpg"></a></div>
-
-<div class="col-md-4 col-sm-4 col-xs-12" style="padding-top:10px"><a href="#" target="_blank"><img alt="Retiro en Tienda o Envío a Domicilio" class="img-responsive" src="productos/Retiro.jpg"></a></div>
-</div>
-
-          <div class="espacio" style="padding:2vw"></div>
-
-    <div  class="container ">
-
-                  <h1 class="productosMas">Lo Mas Buscado de Accesorios <a class="vermas"href="#"> Ver Mas</a></h1>
-    </div>
-    <div class="espacio" style="padding:2vw"></div>
-    <div  class="container">
-      <div id="rowIndex"class="row   ">
-            <div class="col-sm-3 div-img"> <a href="../productos/productos.php"> <img  class="img" class="productoBuscado" src="productos/accesorios/1.png" alt="Helaera Corona"> </a>  <div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-            <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/accesorios/2.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-            <div class="col-sm-3 div-img"><img class="img" class="productoBuscado" src="productos/accesorios/3.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-            <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/accesorios/4.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-      </div>
-    </div>
-    <div class="espacio" style="padding:3vw"></div>
-    <div  class="container">
-      <div id="rowIndex"class="row">
-                <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/5.png" alt="Helaera Corona"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/6.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/7.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/8.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-      </div>
-    </div>
-    <div class="espacio" style="padding:1vw"></div>
-    <div  class="container ">
-
-                  <h1 class="productosMas">Lo Mas Buscado de Porrones <a class="vermas"href="#"> Ver Mas</a></h1>
-    </div>
-
-    <div class="espacio" style="padding:1vw"></div>
-
-    <div  class="container">
-        <div id="rowIndex"class="row">
-                    <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron1.png" alt="Helaera Corona"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                    <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron2.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                    <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron3.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                    <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron4.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+  </head>
+  <body>
+  <?php require "../Nav/nav.php" ?>
+      <section id="seccion">
+        <div id="promo" class="row">
+          <div class="col-md-4 col-sm-4 col-xs-12" id="columna" style="padding-top:10px"><a href="#" target="_blank"><img alt="Financiación" class="img-responsive" src="productos/Financiacion.jpg"></a></div>
+          <div class="col-md-4 col-sm-4 col-xs-12" id="columna" style="padding-top:10px"><a href="#" target="_blank"><img alt="Video instructivo" class="img-responsive" src="productos/tutorial.jpg"></a></div>
+          <div class="col-md-4 col-sm-4 col-xs-12" id="columna" style="padding-top:10px"><a href="#" target="_blank"><img alt="Retiro en Tienda o Envío a Domicilio" class="img-responsive" src="productos/Retiro.jpg"></a></div>
         </div>
-    </div>
-    <div class="espacio" style="padding:3vw"></div>
-    <div  class="container">
-      <div id="rowIndex"class="row">
-                        <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron5.png" alt="Helaera Corona"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                        <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron6.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                        <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron7.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-                        <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron8.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
-      </div>
-    </div>
-      <div class="espacio" style="padding-top:8vw"></div>
-</section>
-      <?php require "../Footer/Footer.php" ?>
+        <div class="espacio" style="padding:2vw"></div>
+          <div  class="container ">
+            <h1 class="productosMas">Lo Mas Buscado de Accesorios <a class="vermas"href="#"> Ver Mas</a></h1>
+          </div>
+        <div class="espacio" style="padding:2vw"></div>
+        <div  class="container">
+          <div id="rowIndex"class="row   ">
+                <div class="col-sm-3 div-img"> <a href="../productos/productos.php"> <img  class="img" class="productoBuscado" src="productos/accesorios/1.png" alt="Helaera Corona"> </a>  <div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+                <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/accesorios/2.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+                <div class="col-sm-3 div-img"><img class="img" class="productoBuscado" src="productos/accesorios/3.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+                <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/accesorios/4.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+          </div>
+        </div>
+        <div class="espacio" style="padding:3vw"></div>
+        <div  class="container">
+          <div id="rowIndex"class="row">
+            <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/5.png" alt="Helaera Corona"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/6.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/7.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            <div class="col-sm-3 div-img"><img  class="img"  class="productoBuscado" src="productos/accesorios/8.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+          </div>
+        </div>
+        <div class="espacio" style="padding:1vw"></div>
+        <div  class="container ">
+          <h1 class="productosMas">Lo Mas Buscado de Porrones <a class="vermas"href="#"> Ver Mas</a></h1>
+        </div>
+        <div class="espacio" style="padding:1vw"></div>
+        <div  class="container">
+            <div id="rowIndex"class="row">
+              <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron1.png" alt="Helaera Corona"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+              <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron2.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+              <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron3.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+              <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron4.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            </div>
+        </div>
+        <div class="espacio" style="padding:3vw"></div>
+        <div  class="container">
+          <div id="rowIndex"class="row">
+            <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron5.png" alt="Helaera Corona"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron6.png" alt="vasos termicos"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron7.png" alt="Tapas goma"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+            <div class="col-sm-3 div-img"><img  class="img" class="productoBuscado" src="productos/porrones/porron8.png" alt="estuche latita"><div class="">$58.00</div> <button type="button" class="btn btn-primary">Agregar al Carrito </button></div>
+          </div>
+        </div>
+        <div class="espacio" style="padding-top:8vw"></div>
+    </section>
+    <?php require "../Footer/Footer.php" ?>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-   <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
-   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-   <script src="https://use.fontawesome.com/releases/v5.10.2/js/all.js" data-auto-replace-svg="nest"></script>
-   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://use.fontawesome.com/releases/v5.10.2/js/all.js" data-auto-replace-svg="nest"></script>
   </body>
 </html>
